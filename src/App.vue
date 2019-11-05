1066c47f9a672459d13e4c6eb1fce4de7baf5973
Jeremy <me@jeremypoole.ca>
2019-11-04 19:27:03 -0800

    delete commented-out code and unneeded data properties

--- src/App.vue
+++ src/App.vue
@@ -24,39 +24,19 @@
 
 <script>
 import Searchbox from "./components/search.vue";
-// import Torrents from './components/torrents.vue'
-// import Controls from './components/controls.vue'
-// import Latest from './components/latest.vue'
 import Error from './components/error.vue'
 import Speechbubble from './components/speechBubble.vue'
-
-const dataObj = {
-//   baseUrl: 'https://eztv.io/api/get-torrents',
-//   api: {
-//     limit: 0,
-//     page: 0,
-//     torrents: [],
-//     torrent_count: 0
-//   },
-  state: {
-//     page: 1,
-//     limit: 40,
-    error: '',
-    speech: 'Meowcats slays the ads!',
-    loading: false
-  },
-//   setPage: function(pageNumber) {
-//     this.state.page = pageNumber
-//   },
-//   setLimit: function(limitNumber) {
-//     this.state.limit = limitNumber
-//   },
-};
 
 export default {
   name: 'app',
   data: function() {
-    return dataObj
+    return {
+      state: {
+        error: '',
+        speech: 'Meowcats slays the ads!',
+        loading: false
+      }
+    }
   },
   components: {
     Error,
@@ -75,57 +55,7 @@ export default {
       }
     },
   },
-  created: function() {
-    // this.fetchAndUpdate(this.api)
-  },
-  mounted: function() {
-    // handle edge case where window is reloaded while scrolled down
-    // this.ensureWindowIsNearTop()
-  },
   methods: {
-    // ensureWindowIsNearTop: function() {
-    //   // if the user is scrolled past the first li element, scroll to top
-    //   if (window.pageYOffset > 211) {
-    //     window.scroll(0,0);
-    //   }
-    // },
-    // encodeParams: (obj) => (!obj) ? '' :
-    // // if no obj is passed in, return an empty string.
-    // // else return url encoded params
-    //   ( 
-    //     "?" + 
-    //     Object.keys(obj).map( k => 
-    //       `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`
-    //       )
-    //       .join('&') 
-    //   ),
-    // fetchJSON: async function(params) {
-    //   const url = this.baseUrl + this.encodeParams(params);
-    //   try {
-    //     const response = await fetch(url);
-    //     const json = await response.json();
-    //     return json;
-    //   }
-    //   catch (e) {
-    //     this.setError("The service is currently unreachable");
-    //     return;
-    //   }
-    // },
-    
-    // fetchAndUpdate: async function(dataObj = this.api, params) {
-    //   this.setLoading(true);
-    //   this.ensureWindowIsNearTop();
-    //   const json = await this.fetchJSON(params);
-    //   if (json) {
-    //     if (json.torrents_count > 0) {
-    //       Object.assign(dataObj, json);
-    //       this.setError('');
-    //     } else {
-    //       this.setError('Sorry, no torrents found :(');
-    //     }
-    //   }
-    //   this.setLoading(false);
-    // },
     setError: function(msg) {
       this.state.error = msg
     },
