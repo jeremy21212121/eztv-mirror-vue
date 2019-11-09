a2f7fdc53423196f0727c85f41bb3b489d867429
Jeremy <me@jeremypoole.ca>
2019-11-08 20:56:00 -0800

    modify initializeStore mutation to handle loading a variety of things from localStorage, including myShows for myShows feature

--- src/store/index.js
+++ src/store/index.js
@@ -5,20 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    resultsPerPage: 40
+    resultsPerPage: 40,
+    myShows: []
   },
   actions: {
   },
   mutations: {
     initializeStore(state) {
-      try {
-        const rpp = parseInt(localStorage.getItem('rpp'))
-        if (rpp > 19 && rpp < 51) {
-          state.resultsPerPage = rpp
+      // values being persisted to localStorage
+      const toInitialize = [
+        {
+          name: 'rpp',
+          stateName: 'resultsPerPage',
+          type: 'number',
+          validation: (n) => n > 19 && n < 51
+        },
+        {
+          name: 'myShows',
+          stateName: 'myShows',
+          type: 'json',
+          validation: () => true
         }
-      } catch (error) {
-        // swallow error
-      }
+      ]
+      toInitialize.forEach(init => {
+        // get correct parser for type. If type is not recognized, it will return input type
+        const parser = init.type === 'number' ? parseInt : init.type === 'json' ? JSON.parse : (x) => x
+        try {
+          const item = parser(localStorage.getItem(init.name));
+          if (init.validation(item)) {
+            state[init.stateName] = item
+          }
+        } catch (error) {
+          // swallow error cuz localStorage is optional
+        }
+      })
+      // try {
+      //   const rpp = parseInt(localStorage.getItem('rpp'))
+      //   if (rpp > 19 && rpp < 51) {
+      //     state.resultsPerPage = rpp
+      //   }
+      // } catch (error) {
+      //   // swallow error
+      // }
     },
     setResultsPerPage(state, number) {
       state.resultsPerPage = number
